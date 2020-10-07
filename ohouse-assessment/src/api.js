907b52a38a27f617ef666a7ee776346d4b52f855
Rachel4858 <rachel458458@gmail.com>
2020-10-07 22:12:00 +0900

    [add][test]api요청 후 테스트: - axios사용해서 api요청 후 console에 찍어봄

---
+++ ohouse-assessment/src/api.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+//
+// // page_1.json
+// const base_url = "https://bucketplace-coding-test.s3.amazonaws.com/cards/";
+//
+// export const houseListApi = {
+//     houseList: () => axios.get(`${base_url}page_1.json`)
+//
+// };
+//
--- ohouse-assessment/src/components/HouseListPage/HouseListContainer/HouseListContainer.jsx
+++ ohouse-assessment/src/components/HouseListPage/HouseListContainer/HouseListContainer.jsx
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+import axios from "axios";
 import "./HouseListContainer.scss";
 import HouseItem from "./HouseItem/HouseItem";
 
 
 export default class HouseListContainer extends Component {
+
+    state = {
+        data: null
+    };
+
+    componentDidMount() {
+        const base_url = "https://bucketplace-coding-test.s3.amazonaws.com/cards/";
+        axios.get(`${base_url}page_1.json`)
+            .then(response => {
+                this.setState({
+                    data: response.data
+                });
+            }).then(() => {
+            console.log(this.state.data)
+        })
+    }
+
+
+
+
     render() {
+        const { data } = this.state;
+
         return (
             <div className="house-list-wrap">
+
                 <HouseItem/>
                 <HouseItem/>
                 <HouseItem/>
